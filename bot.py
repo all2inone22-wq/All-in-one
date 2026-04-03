a4a0ffc47fea9c3ac5fcbd17d0d312c94db8f493
all2inone22-wq <all2inone22@gmail.com>
2026-04-03 18:53:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ async def dreamxbotz_start():
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
-    web.TCPSite(app, bind_address, 8080).start()
+    await web.TCPSite(app, bind_address, 8080).start()
     dreamxbotz.loop.create_task(keep_alive())
     await idle()
     
