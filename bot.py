cb7f404f2ba1a64a872dc8dd36159868b1b49297
all2inone22-wq <all2inone22@gmail.com>
2026-04-03 21:54:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ async def dreamxbotz_start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
-    await dreamxbotz.send_message(chat_id=LOG_CHANNEL, text=script.START_TXT.format(temp.B_LINK, today, time))
+    await dreamxbotz.send_message(chat_id=LOG_CHANNEL, text="Bot Successfully Started!")
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
