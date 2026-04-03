3a48fad23de4d0ff3b5559bf55d99a5319253c3d
all2inone22-wq <all2inone22@gmail.com>
2026-04-03 13:55:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,3 +67,4 @@ TMDB_API_KEY = environ.get('TMDB_API_KEY', '')
 Bot_cmds = []
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS', 0))
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', 0))
+AUTH_REQ_CHANNELS = environ.get('AUTH_REQ_CHANNELS', '')
